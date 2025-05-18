5db872400762845813bc0610f9cc328ef478f7ac
Hakaze S <frost.scarlet.163@gmail.com>
2025-05-18 22:08:53 +0300

    first commit

---
+++ src/test/java/github/com/imageservice/ImageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.imageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
