226ea8c3c23df765cad6710df222e401589d55c3
Hakaze S <frost.scarlet.163@gmail.com>
2025-06-01 06:19:51 +0300

    Implemented saving the image to temp directory

--- src/main/java/github/com/pairimagestorage/ImageSender.java
+++ src/main/java/github/com/pairimagestorage/ImageSender.java
@@ -1,6 +1,5 @@
 package github.com.pairimagestorage;
 
-import github.com.pairimagestorage.exception.IOResourceException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -9,6 +8,10 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
 
 @Component
 public class ImageSender {
@@ -20,19 +23,55 @@ public class ImageSender {
     }
 
     public void sendRequest(MultipartFile image) {
-        try {
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("http://localhost:5000/convert_image"))
-                    .headers("Content-Type", image.getContentType())
-                    .POST(HttpRequest.BodyPublishers.ofByteArray(image.getBytes()))
-                    .build();
+        Path path = Paths.get("temp");
+        if (Files.notExists(path)) {
+            try {
+                Files.createDirectory(path);
+            } catch (IOException e) {
+                throw new DirectoryCreatingException("Failed to create directory /temp", e);
+            }
 
-            httpClient.send(request, HttpResponse.BodyHandlers.ofByteArray());
-        } catch (IOException e) {
-            throw new IOResourceException("Invalid request form or unable to extract data from response", e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            Path tempDir = path
+                    .resolve(Objects.requireNonNull(image.getOriginalFilename()))
+                    .normalize()
+                    .toAbsolutePath();
+            try {
+                Files.copy(image.getInputStream(), tempDir);
+            } catch (IOException e) {
+                throw new ImageSavingException("Unable to save image " + image.getName(), e);
+            }
+
+            try {
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create("http://localhost:5000/convert_image"))
+                        .headers("Content-Type", image.getContentType())
+                        .POST(HttpRequest.BodyPublishers.ofFile(tempDir))
+                        .build();
+
+                httpClient.send(request, HttpResponse.BodyHandlers.ofByteArray());
+            } catch (IOException e) {
+                throw new IOResourceException("Unable to extract data from response or send request to server", e);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
+    }
 
+    private static class DirectoryCreatingException extends RuntimeException {
+        public DirectoryCreatingException(String message, Exception e) {
+            super(message, e);
+        }
+    }
+
+    private static class ImageSavingException extends RuntimeException {
+        ImageSavingException(String message, Exception e) {
+            super(message, e);
+        }
+    }
+
+    private static class IOResourceException extends RuntimeException {
+        public IOResourceException(String message, Exception e) {
+            super(message, e);
+        }
     }
 }
--- src/main/java/github/com/pairimagestorage/exception/IOResourceException.java
+++
@@ -1,7 +0,0 @@
-package github.com.pairimagestorage.exception;
-
-public class IOResourceException extends RuntimeException{
-    public IOResourceException(String message, Exception e) {
-        super(message, e);
-    }
-}
